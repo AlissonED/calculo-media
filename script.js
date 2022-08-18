f6eff48f1255cf55d31d14d7e3a0585b30794dbb
Alisson Evangelista <108416106+AlissonED@users.noreply.github.com>
2022-08-18 12:18:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+  function calcSum() {
+    let num1 = document.getElementsByName("nt")[0].value;
+    let num2 = document.getElementsByName("nq")[0].value;
+    let num3 = document.getElementsByName("npart")[0].value;
+    let num4 = document.getElementsByName("nprova")[0].value;
+    let sum = ((Number(num1) + Number(num2) + Number(num3))/3)* 0.4 + (Number(num4)* 0.6);
+    document.getElementsByName("media")[0].value = sum;
+}
