547006005f0ec0fbf746914b3a0da20bf6c60a5a
Alisson Evangelista <108416106+AlissonED@users.noreply.github.com>
2022-08-19 11:33:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,4 +6,12 @@
     let num4 = document.getElementsByName("nprova")[0].value;
     let sum = ((Number(num1) + Number(num2) + Number(num3))/3)* 0.4 + (Number(num4)* 0.6);
     document.getElementsByName("media")[0].value = sum;
+
+  if (sum>=6) {
+    document.getElementsByName("ar")[0].value = "Aprovado";
+    document.getElementById("ar").style.color = "green";
+  } else {
+    document.getElementsByName("ar")[0].value = "Reprovado";
+    document.getElementById("ar").style.color = "red";
+  }
 }
